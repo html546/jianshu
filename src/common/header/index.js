db9b501651c828db8360bfe75962b3fde8b45213
chengjiang <15712859926@163.com>
2018-10-15 16:54:17 +0800

    使用react-immutable.js

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -57,7 +57,7 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.get('focused')
+        focused: state.getIn(['header', 'focused'])
     }
 }
 const mapDispatchToProps = (dispatch) => {
