f91277de807ccd3ed7ab03391400319d80173776
chengjiang <15712859926@163.com>
2018-10-16 11:23:29 +0800

    首页组件的拆分

---
+++ src/pages/home/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import {
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './style';
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-img' src="//upload.jianshu.io/admin_banners/web_images/4448/d9a0fd8f0622d2323161b68d2123c7f28837d28f.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+                    <Topic />
+                    <List></List>
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+};
+export default Home;
