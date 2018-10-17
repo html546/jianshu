0002953b3da0e39e81fe9040f5e3f0e2fdd1a58f
chengjiang <15712859926@163.com>
2018-10-17 11:06:12 +0800

    页面路由参数的传递

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -20,7 +20,7 @@ class Detail extends Component {
         )
     }
     componentDidMount() {
-        this.props.getDetail();
+        this.props.getDetail(this.props.match.params.id);
     }
 };
 const mapState = (state) => ({
@@ -28,8 +28,8 @@ const mapState = (state) => ({
     content: state.getIn(['detail', 'content'])
 })
 const mapDispatch = (dispatch) => ({
-    getDetail(){
-        dispatch(actionCreators.getDetail())
+    getDetail(id){
+        dispatch(actionCreators.getDetail(id))
     }
 })
 export default connect(mapState, mapDispatch)(Detail);
